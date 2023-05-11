83a2c492443aaf7b30a65bec4f55baef8076b58a
Andreas Avgoustis <andreas_ker@outlook.com.gr>
2023-05-11 15:45:05 +0300

    Add files via upload

---
+++ Smoothing Moving Average Times Series/Smoothing Moving Average for Wiener Process.py
@@ -0,0 +1,95 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import csv
+
+def generate_random_data(size):
+    w1 = np.zeros(size)
+    w2 = np.zeros(size)
+    for i in range(size):
+        w1[i] = round(random.uniform(-1.0, 1.0), 1)
+        w2[i] = round(random.uniform(-1.0, 1.0), 1)
+    return w1, w2
+
+def write_data_to_csv(filename, w1, w2):
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['w1', 'w2'])
+        for i in range(len(w1)):
+            writer.writerow([w1[i], w2[i]])
+
+def apply_smoothing_moving_average(original_series, window_size):
+    smoothed_series = np.convolve(original_series, np.ones(window_size) / window_size, mode='valid')
+    return smoothed_series
+
+# Generate random data
+size = 1000
+w1, w2 = generate_random_data(size)
+
+# Write the generated data to a CSV file
+write_data_to_csv('OData.csv', w1, w2)
+
+# Apply smoothing moving average to t1
+window_size = 15  # Adjust the window size as needed
+t1 = np.zeros(size)
+t2 = np.zeros(size)
+s1 = 2
+s2 = 120
+
+for i in range(1, size):
+    t1[i] = t1[i-1] + s1 * np.sqrt(1 / size) * w1[i]
+
+# Smooth t1 using moving average
+smoothed_t1 = apply_smoothing_moving_average(t1, window_size)
+
+# Plotting the original and smoothed series for t1
+plt.figure(figsize=(20, 12))
+plt.plot(t1, label='Original t1')
+plt.plot(smoothed_t1, label='Smoothed t1')
+plt.xlabel('Time')
+plt.ylabel('Value')
+plt.legend()
+plt.show()
+
+# Calculate differences for t1
+diff1 = [t1[i] - t1[i-1] for i in range(1, len(t1))]
+
+for i in range(1, size):
+    t2[i] = t2[i-1] + s2 * np.sqrt(1 / size) * w2[i]
+
+# Smooth t2 using moving average
+smoothed_t2 = apply_smoothing_moving_average(t2, window_size)
+
+# Plotting the original and smoothed series for t2
+plt.figure(figsize=(20, 12))
+plt.plot(t2, label='Original t2')
+plt.plot(smoothed_t2, label='Smoothed t2')
+plt.xlabel('Time')
+plt.ylabel('Value')
+plt.legend()
+plt.show()
+
+# Calculate differences for t2
+diff2 = [t2[i] - t2[i-1] for i in range(1, len(t2))]
+
+# Smooth the differences using moving average
+smoothed_diff1 = apply_smoothing_moving_average(diff1, window_size)
+smoothed_diff2 = apply_smoothing_moving_average(diff2, window_size)
+
+# Plotting the original and smoothed differences for t1
+plt.figure(figsize=(20, 12))
+plt.plot(diff1, label='Original diff1')
+plt.plot(smoothed_diff1, label='Smoothed diff1')
+plt.xlabel('Time')
+plt.ylabel('Value')
+plt.legend()
+plt.show()
+
+# Plotting the original and smoothed differences for t2
+plt.figure(figsize=(20, 12))
+plt.plot(diff2, label='Original diff2')
+plt.plot(smoothed_diff2, label='Smoothed diff2')
+plt.xlabel('Time')
+plt.ylabel('Value')
+plt.legend()
+plt.show()
